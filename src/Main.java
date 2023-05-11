14e3db5d71f9ae9458755e2bf159e5651ebaa713
Georgi Ivanov <114055119+georgiValentinovIvanov@users.noreply.github.com>
2023-05-11 09:05:24 +0300

    update main class message

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        printTrianglePerimeterInput();
+        printQuadraticEquation();
     }
-    public static void printTrianglePerimeterInput() {
-        Scanner myObj = new Scanner(System.in);
+    public static void printQuadraticEquation() {
+        // value of the constants a, b, c
+        double a = 7.2;
+        double b = 5;
+        double c = 9;
 
-        System.out.println("Enter side a size");
-        int a = myObj.nextInt();
+        // declared the two roots
+        double root1;
+        double root2;
 
-        System.out.println("Enter side b size");
-        int b = myObj.nextInt();
+        // determinant (b^2 - 4ac)
+        double determinant = b * b - 4 * a * c;
 
-        System.out.println("Enter side c size");
-        int c = myObj.nextInt();
+        System.out.println("Determinant = " + determinant);
 
-        int perimeter = a + b + c;
+        //determinant > 0
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+        root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+        System.out.println("\nDeterminant > 0");
+        System.out.println("root1 = " + root1);
+        System.out.println("root2 = " + root2);
 
-        System.out.println("Triangle Perimeter is: " + perimeter);
+
+        //determinant == 0
+        root1 = root2 = -b / (2 * a);
+        System.out.println("\nDeterminant == 0");
+        System.out.println("root1 = root2 = " + root1);
+
+        //determinant < 0
+        double real = -b / (2 * a);
+        double imaginary = Math.sqrt(-determinant) / (2 * a);
+        System.out.println("\nDeterminant < 0");
+        System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+        System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
     }
 }
