501703dab40ecb6fcc88c2e1db4811479bebd88b
Georgi Ivanov <114055119+georgiValentinovIvanov@users.noreply.github.com>
2023-05-11 09:04:18 +0300

    update main class message

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        printPineTree();
+        printTrianglePerimeterInput();
     }
-    public static void printPineTree() {
-        System.out.println("  *");
-        System.out.println(" ***");
-        System.out.println("*****");
+    public static void printTrianglePerimeterInput() {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter side a size");
+        int a = myObj.nextInt();
+
+        System.out.println("Enter side b size");
+        int b = myObj.nextInt();
+
+        System.out.println("Enter side c size");
+        int c = myObj.nextInt();
+
+        int perimeter = a + b + c;
+
+        System.out.println("Triangle Perimeter is: " + perimeter);
     }
 }
