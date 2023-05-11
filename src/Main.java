1dd4431a915978bdf3b3a4e7a5b6d59084d89fc6
Georgi Ivanov <114055119+georgiValentinovIvanov@users.noreply.github.com>
2023-05-11 08:54:07 +0300

    update main class message

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome my first GIT!");
+        System.out.printf("Hello and welcome my first github!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
